e0860e110999a9abb5363cada0a4a278d13fd3c3
bankay <sanjikflaxshback@mail.ru>
2025-03-13 21:27:11 +0700

    Update tringle.cpp

--- tringle.cpp
+++ tringle.cpp
@@ -31,20 +31,45 @@ int main(void)
     double a, b, c;
     std::cout << "Введите стороны треугольника: ";
     std::cin >> a >> b >> c;
-    if (!isTriangle(a, b, c)) // Проверка треугольника на существование
+    if (a <= 0 || b <= 0 || c <= 0)
     {
-       std::cout << "Некорреткные стороны треугольника" << std::endl;
-       return 1;
+        std::cout << "Ошибка: Введены некорректные значения." << std::endl;
     }
-    else
+    else 
     {
-        std::cout << "Периметр: " << perimeter(a, b, c) << std::endl;
-        std::cout << "Площадь: " << triangleGeron(a, b, c) << std::endl;
-
-        if (isIsosceles(a, b, c)) 
-            std::cout << "Треугольник равнобедренный" << std::endl;
-        else 
-            std::cout << "Треугольник неравнобедренный" << std::endl;
-    }   
+        if (!isTriangle(a, b, c)) // Проверка треугольника на существование
+        {
+        std::cout << "Некорреткные стороны треугольника." << std::endl;
+        return 1;
+        }
+        else
+        {
+            int opt;
+            std::cout << "Меню выбора: \n 1) периметр \n 2) площадь \n 3) проверка на равнобедренность" << std::endl;
+            std::cin >> opt;
+            switch (opt)
+            {
+            case 1:
+            {
+                std::cout << "Периметр: " << perimeter(a, b, c) << std::endl;
+                break;
+            }
+            case 2:
+            {
+                std::cout << "Площадь: " << triangleGeron(a, b, c) << std::endl;
+            }
+            case 3: 
+            {
+                if (isIsosceles(a, b, c)) 
+                    std::cout << "Треугольник равнобедренный" << std::endl;
+                else 
+                    std::cout << "Треугольник неравнобедренный" << std::endl;
+            }
+            default: 
+                std::cout << "Ошибка" << std::endl;
+                break;
+            }    
+        }  
+    } 
     return 0;
 }
