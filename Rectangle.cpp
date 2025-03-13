1a0c48261e771aade6c02d7fe47e5c87fce82fff
Stavr Sharun <stavr_sd@mail.ru>
2025-03-13 19:23:07 +0700

    Added Rectangle

---
+++ Rectangle.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+// Вычисление периметра прямоугольника
+double rectPerim(double l, double w) 
+{
+    return 2 * (l + w);
+}
+
+// Вычисление площади прямоугольника
+double rectArea(double l, double w) 
+{
+    return l * w;
+}
+
+// Вычисление длины диагонали прямоугольника
+double rectDiag(double l, double w) 
+{
+    return sqrt(l * l + w * w);
+}
+
+int main() 
+{
+    double l, w;
+    cout << "Введите длину и ширину прямоугольника через пробел: ";
+    cin >> l >> w;
+    
+    if (l <= 0 || w <= 0) 
+{
+        cout << "Некорректные размеры прямоугольника." << endl;
+        return 1;
+    }
+    
+    cout << "Периметр прямоугольника: " << rectPerim(l, w) << endl;
+    cout << "Площадь прямоугольника: " << rectArea(l, w) << endl;
+    cout << "Длина диагонали прямоугольника: " << rectDiag(l, w) << endl;
+    
+    return 0;
+}
